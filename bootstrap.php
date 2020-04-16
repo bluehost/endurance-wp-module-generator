f81a2c4a35851f7365714945da343f394afe7a56
Micah Wood <micah@wpscholar.com>
2020-04-16 19:03:02 -0400

    Initial commit

---
+++ bootstrap.php
@@ -0,0 +1,47 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+// Ascertain the module slug and name
+$module_slug = str_replace( 'endurance-wp-module-', '', basename( __DIR__ ) );
+$module_name = ucwords( str_replace( '-', ' ', $module_slug ) );
+
+// Prepare template string replacements
+$replacements = [
+	'moduleName'  => $module_name,
+	'modulename'  => strtolower( $module_name ),
+	'moduleSlug'  => $module_slug,
+	'module_slug' => str_replace( '-', '_', $module_slug ),
+];
+
+// Write files
+$mustache = new Mustache_Engine();
+
+$templates = new RecursiveDirectoryIterator( __DIR__ . '/.templates', RecursiveDirectoryIterator::SKIP_DOTS );
+foreach ( $templates as $file ) {
+	$contents = $mustache->render( file_get_contents( $file->getRealPath() ), $replacements );
+	file_put_contents( __DIR__ . '/' . str_replace( '.mustache', '', $file->getFilename() ), $contents );
+}
+
+file_put_contents( __DIR__ . '/' . $module_slug . '.php', '<?php ' . PHP_EOL );
+
+// Clean up
+unlink( __DIR__ . '/composer.lock' );
+deleteDir( __DIR__ . '/.templates' );
+deleteDir( __DIR__ . '/vendor' );
+
+/**
+ * Recursively delete everything in a directory.
+ *
+ * @param string $dir Directory to be deleted
+ *
+ * @return bool
+ */
+function deleteDir( $dir ) {
+	$files = array_diff( scandir( $dir ), array( '.', '..' ) );
+	foreach ( $files as $file ) {
+		is_dir( "$dir/$file" ) ? deleteDir( "$dir/$file" ) : unlink( "$dir/$file" );
+	}
+
+	return rmdir( $dir );
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Endurance WordPress Module Generator
+
+## Use
+
+To create a new Endurance WordPress module, run:
+
+```
+composer create-project bluehost/endurance-wp-module-template endurance-wp-module-<module-name> --repository-url=https://bluehost.github.io/satis/
+```
+
+Be sure to replace `<module-name>` in the command above with the actual name of the new module in slug form.
